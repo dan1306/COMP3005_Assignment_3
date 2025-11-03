b01e86aaced2b4890f27f1a17b9cd1757bb00866
Daniel Sakyi <dannysakyi@gmail.com>
2025-11-03 16:29:17 -0500

    rough code draft with all main funcionality

--- db/index.ts
+++ db/index.ts
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 export const pool = new Pool({
   user: "postgres",        // your postgres username
   host: "localhost",
-  database: "secon_lib_system",      // your database name
+  database: process.env.database_name,      // your database name
   password: process.env.database_password,
   port: 5432,
 });
--- main.ts
+++ main.ts
@@ -2,6 +2,13 @@ require('dotenv').config();
 
 const readline = require("readline-sync");
 
+interface addStudent {
+  first_name: string; 
+  last_name: string; 
+  email: string; 
+  enrollment_date: Date;
+}
+
 // while (true) {
 //   const input = readline.question("Enter something (or type exit): ");
 
@@ -15,10 +22,33 @@ const readline = require("readline-sync");
 
 const { pool } = require("./db/index");
 
-async function testDB() {
-  const result = await pool.query("SELECT * FROM authors;");
+async function main() {
+  const result = await pool.query("SELECT * FROM students;");
   console.log(result.rows);
 }
 
-testDB();
+async function getAllStudents(){
+  const result = await pool.query("SELECT * FROM students;");
+  console.log(result.rows); 
+}
+
+async function addStudent({first_name, last_name, email, enrollment_date}:addStudent) {
+  await pool.query(
+    `INSERT INTO students (first_name, last_name, email, enrollment_date)
+     VALUES ($1, $2, $3, $4)`,
+    [first_name, last_name, email, enrollment_date]
+  );
+}
+
+async function updateStudentEmail(student_id, new_email) {
+await pool.query("UPDATE students SET email = $1 WHERE student_id = $2"),
+[student_id, new_email]
+} 
+
+async function deleteStudent(student_id){
+  await pool.query("DELETE FROM students WHERE student_id = $1",
+    [student_id]
+  )
+}
+main();
 
